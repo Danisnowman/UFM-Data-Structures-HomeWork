206b84f48b3b24166b996f7752d336f5a5995443
danisnowman <danisnowman@gmail.com>
2019-01-28 16:05:17 -0600

    Changed printf() to std::cout << text;

---
+++ Tarea3DynamicArray/Tarea3DynamicArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tarea3DynamicArray
+//
+//  Created by Daniel Hernández on 1/28/19.
+//  Copyright © 2019 Daniel Hernández. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
