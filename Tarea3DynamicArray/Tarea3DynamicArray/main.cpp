733c2f1117663a538b7ce65528e004e5b5d9c57d
danisnowman <danisnowman@gmail.com>
2019-01-28 18:02:59 -0600

    Initial development of "Tarea3D"

--- Tarea3DynamicArray/Tarea3DynamicArray/main.cpp
+++ Tarea3DynamicArray/Tarea3DynamicArray/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
+#include "commonFunctions.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int choice, data;
+
+int main(){
+while(!done){
+    /* Menu */
+    printf("\n1. Push\n2. Pop\n3. Clear\n4. Show\n5. Exit\n");
+    choice = askInt("\nEnter your option: ", 5);
+    
+    switch(choice){
+            /// Push
+        case 1:
+            int data = askInt("Enter data to push into stack: ", INT_MAX);
+            
+            /// Push item to stack
+            push(data);
+            break;
+            /// Pop
+        case 2:
+            data = pop();
+            
+            /// If stack is not empty
+            if (data != INT_MIN)
+                printStack();
+                break;
+            
+            /// Clear
+        case 3:
+            clearArray();
+            break;
+            /// Exit
+        case 4:
+            printStack();
+            break;
+        case 5:
+            printf("\nExiting from app.");
+            exit(0);
+            break;
+            
+            /// Size
+            
+            
+        default:
+            printf("\nInvalid choice, please try again.\n");
+    }
+    
+    printf("\n\n");
+}
+
+
+return 0;
 }
---
+++ Tarea3DynamicArrayInC/Tarea3DynamicArrayInC/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Tarea3DynamicArrayInC
+//
+//  Created by Daniel Hernández on 1/28/19.
+//  Copyright © 2019 Daniel Hernández. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "commonFunctions.h"
+#include "Stack.h"
+
+int main(){
+    
+}
