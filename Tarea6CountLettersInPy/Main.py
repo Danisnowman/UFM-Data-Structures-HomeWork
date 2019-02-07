4cd7454f2347f3e5871c2ba15cc49c8854d02a9e
danisnowman <danisnowman@gmail.com>
2019-02-06 20:54:58 -0600

    Initial Release Tarea6

--- Tarea6CountLettersInPy/Main.py
+++ Tarea6CountLettersInPy/Main.py
@@ -1,7 +1,41 @@
-from string import ascii_lowercase
-from collections import Counter
+import operator
 
-with open('text.txt') as f:
-    print Counter(letter for line in f
-                  for letter in line.lower()
-                  if letter in ascii_lowercase)
+
+# class Main:
+#    def __init__(self, text, filename, txt_file):
+#        self.count = 0
+#        self.text = text
+#        self.filename = filename
+#        self.txt_file = txt_file
+
+# def count_a_letter(text, letter):
+#    count = 0
+#   for c in text:
+#        count += (c == letter)
+#    return count
+
+# fileToRead = open("test.txt")
+# print("A: {0}".format(str(count_a_letter(fileToRead.read(), "a"))))
+
+def count_letters(filename):
+    letters = {}
+    for n in filename:
+        keys = letters.keys()
+        if n in keys:
+            letters[n] += 1
+        else:
+            letters[n] = 1
+    return letters
+
+
+def read_and_sort(txt_file):
+    with open(txt_file, 'r') as file_to_read:
+        file_to_read = file_to_read.read()
+        # file_to_read = [line.replace(' ', '') for line in file_to_read]
+
+    unsorted_dictionary = count_letters(file_to_read)
+    for key, value in reversed(sorted(unsorted_dictionary.items(), key=operator.itemgetter(1))):
+        print(str(key).upper() + ":", value, int(value) * '*')
+
+
+read_and_sort('test.txt')
