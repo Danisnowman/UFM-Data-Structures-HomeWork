21d0f636b1412ff26c38f41794d82465fd0c46f5
danisnowman <danisnowman@gmail.com>
2019-03-19 09:51:29 -0600

    added unit testing

--- Tarea11BTreeSearch/src/test/java/BTreeSetTest.java
+++ Tarea11BTreeSearch/src/test/java/BTreeSetTest.java
@@ -4,17 +4,29 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-/*
 public class BTreeSetTest {
-    BTreeSet<String> bts = new BTreeSet<>(5);
-    @DataProvider(name = "add()")
-    public static Object[][] add() {
-        return new Object[][] {{1}, {3}};
+    @Test(groups = "All")
+    public void add(){
+        BTreeSet<String> bts = new BTreeSet<>(5);
+        bts.add("test");
+        Assert.assertTrue(bts.contains("test"));
     }
 
-    @Test(dataProvider = "add")
-    public void BTreeSet(String firstParameter, String secondParameters, String other){
-        bts.add(firstParameter);
-        Assert
+    @Test(groups = "All")
+    public void contains(){
+        BTreeSet<String> bts = new BTreeSet<>(5);
+        for (int i = 0; i < 100 ; i++) {
+            bts.add(String.valueOf(i));
+            Assert.assertTrue(bts.contains(String.valueOf(i)));
+        }
     }
-}*/
+
+    @Test(groups = "All")
+    public void containsInvalid(){
+        BTreeSet<String> bts = new BTreeSet<>(5);
+        for (int i = 0; i < 100 ; i++) {
+            bts.add(String.valueOf(i));
+            Assert.assertFalse(bts.contains(String.valueOf((i+2)^25519)));
+        }
+    }
+}
